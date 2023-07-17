7ab0cccef33a0d065ac76b41193d58c043b314d2
Filipe Zapotoczny <98495900+Zapotoczn@users.noreply.github.com>
2023-07-17 19:23:11 -0300

    Details screen Done

--- rocketnotes/src/pages/details/index.jsx
+++ rocketnotes/src/pages/details/index.jsx
@@ -1,21 +1,42 @@
-import { Container, Links } from "./styles"
+import { Container, Links, Content } from "./styles"
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
+import { ButtonText } from '../../components/ButtonText'
 import { Section } from '../../components/Section'
+import { Tag } from '../../components/Tag'
 export function Details() {
 
   return(
     <Container>
-        <Header />
-        
-        <Section title= "Links uteis">
-          <Links>
-            <li><a href="#">https://github.com/zapotoczn</a></li>
-            <li><a href="#">https://github.com/zapotoczn</a></li>
-          </Links>
-        </Section>
+      <Header />
+        <main>
+          <Content>
+            <ButtonText title="Remove Note"/>
 
-        <Button title= "Comeback" />
+            <h1>
+              React Introduction
+            </h1>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam pariatur voluptatem molestias exercitationem nihil, repellendus vitae perferendis repudiandae similique asperiores soluta perspiciatis eveniet consectetur provident necessitatibus modi voluptas esse vel!
+            </p>
+            <Section title= "Links uteis">
+              <Links>
+                <li><a href="#">https://github.com/zapotoczn</a></li>
+                <li><a href="#">https://github.com/zapotoczn</a></li>
+              </Links>
+            </Section>
+
+            <Section title= "Marcadores">
+              <Tag title="express"/>
+              <Tag title="NodeJS"/>
+            </Section>  
+
+
+
+            <Button title= "Comeback" />
+          </Content>
+        </main> 
     </Container>
   )
 }
--- rocketnotes/src/pages/details/styles.js
+++ rocketnotes/src/pages/details/styles.js
@@ -8,7 +8,13 @@ export const Container = styled.div`
     grid-template-rows: 100px auto;
     grid-template-areas: 
     "header"
-    "content"
+    "content";
+
+    > main {
+        grid-area: content;
+        overflow-y: scroll;
+        padding: 64px 0;
+    }
 `;
 
 export const Links = styled.ul`
@@ -22,4 +28,29 @@ list-style: none;
         }
  }
 
- `
+ `; 
+
+ export const Content = styled.div`
+    max-width: 550px;
+    margin: 0 auto;
+
+    display: flex;
+    flex-direction: column;
+
+    > button:first-child {
+        align-self: end;
+    }
+
+    > h1{
+        font-size: 36px;
+        font-weight: 500;
+        padding-top: 64px;
+
+    }
+
+    > p{
+        font-size: 18px;
+        margin-top: 16px;
+        text-align: justify;
+    }
+ `;
